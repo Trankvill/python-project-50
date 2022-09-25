2d6581b7336ebbb3fa220edd3780d5d182b362f4
Konstantin Chupin <chupin.k@yandex.ru>
2022-09-25 21:35:07 +0500

    add gendiff script

---
+++ Makefile
@@ -0,0 +1,17 @@
+
+install:
+	poetry install
+
+build:
+	poetry build
+
+gendiff:
+	poetry run gendiff
+
+publish:
+	poetry publish --dry-run
+
+
+package-install:
+	python3 -m pip install --user dist/*.whl
+
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+            usage='gendiff [-h] [-f FORMAT] first_file second_file',
+            description='Compares two configuration files and shows a difference'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.print_help()
+    args = parser.parse_args()
+    print(args.first_file, args.second_file)
+
+if __name__=='__main__':
+    main()
