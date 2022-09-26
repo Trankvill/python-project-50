14921388f9f7904dadf4b546e2ac7d54b95e01be
Konstantin Chupin <chupin.k@yandex.ru>
2022-09-26 18:23:55 +0500

    update files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,7 +10,7 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.print_help()
+    #parser.print_help()
     args = parser.parse_args()
     print(args.first_file, args.second_file)
 
