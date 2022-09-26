7829f807e01e9e5705fcb44646eb88d0d9fd44f6
Konstantin Chupin <chupin.k@yandex.ru>
2022-09-26 18:37:50 +0500

    add format in gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,9 +10,12 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    #parser.print_help()
+    parser.add_argument(
+    	'-f', '--format', 
+    	help='set format of output'
+    )
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
+    print(args.first_file, args.second_file, args.format)
 
 if __name__=='__main__':
     main()
