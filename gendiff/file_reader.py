a579e18324ed86c198158619b5ea84caaee9a7e3
Konstantin Chupin <chupin.k@yandex.ru>
2022-10-12 18:08:30 +0500

    fix file_reader.py

--- gendiff/file_reader.py
+++ gendiff/file_reader.py
@@ -9,5 +9,7 @@ def read_file(filepath):
 
 def get_format(filepath):
     root, ext = os.path.splitext(filepath)
-    if ext == '.json':
+    if ext == '.yaml' or ext == '.yml':
+        return 'yaml'
+    elif ext == '.json':
         return 'json'
