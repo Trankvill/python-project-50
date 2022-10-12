a808b5f5cb194978a5c856e0ba82203cc737985e
Konstantin Chupin <chupin.k@yandex.ru>
2022-10-12 18:07:50 +0500

    fix data_parser.py

--- gendiff/data_parser.py
+++ gendiff/data_parser.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import json
+import yaml
 
 
 def parse(data, format_):
     if format_ == 'json':
         return json.loads(data)
+    if format_ == 'yaml':
+        return yaml.safe_load(data)
