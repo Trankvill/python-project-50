d846926e66df0366e3338d8fc26dee1c45e83763
Konstantin Chupin <chupin.k@yandex.ru>
2022-10-12 18:09:51 +0500

    fix formatter.py

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -1,7 +1,10 @@
 from gendiff.formatters.json import format_json
-from gendiff.formatters.formats import JSON
+from gendiff.formatters.stylish import format_stylish
+from gendiff.formatters.formats import JSON, STYLISH
 
 
 def format_diff(diff, formatter):
+    if formatter == STYLISH:
+        return format_stylish(diff)
     if formatter == JSON:
         return format_json(diff)
