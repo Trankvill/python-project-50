e8f9d8e870fd1980cac0a9f6556de7096658e9d8
Konstantin Chupin <chupin.k@yandex.ru>
2022-10-13 19:20:10 +0500

    update project to 1.0.0

--- README.md
+++ README.md
@@ -7,8 +7,18 @@
 ### Help output:
 [![asciicast](https://asciinema.org/a/WdmG6G6d1cDVk3SIFNfpjxcy6.svg)](https://asciinema.org/a/WdmG6G6d1cDVk3SIFNfpjxcy6)
 
-### Comparison of flat files (JSON):
+### Comparison of flat files (FORMAT: JSON):
 [![asciicast](https://asciinema.org/a/YCJ7YK4InOiucts38SDvAghQm.svg)](https://asciinema.org/a/YCJ7YK4InOiucts38SDvAghQm)
 
+<<<<<<< HEAD
 ### Flat file comparison (YAML, YML):
+=======
+### Flat file comparison (FORMATS: YAML, YML):
+>>>>>>> a669c80 (update project to 1.0.0, add file1_nested.json file2_nested.json file1_nested.yaml file2_nested.yaml answer_stylish_nested)
 [![asciicast](https://asciinema.org/a/SCGHFGtLfBNVip5Vyywcblwip.svg)](https://asciinema.org/a/SCGHFGtLfBNVip5Vyywcblwip)
+
+### Recursive file comparison(FORMAT: JSON):
+[![asciicast](https://asciinema.org/a/86cvPE45GuX4mTzjJyrOFeDuS.svg)](https://asciinema.org/a/86cvPE45GuX4mTzjJyrOFeDuS)
+
+### Recursive file comparison(FORMAT: YAML):
+[![asciicast](https://asciinema.org/a/ZFxD90goMVC2YweGXLB5xHVBE.svg)](https://asciinema.org/a/ZFxD90goMVC2YweGXLB5xHVBE)
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -10,10 +10,13 @@ FLAT_YAML_FILE1 = 'tests/fixtures/file1.yaml'
 FLAT_YAML_FILE2 = 'tests/fixtures/file2.yaml'
 FLAT_YML_FILE1 = 'tests/fixtures/file1.yml'
 FLAT_YML_FILE2 = 'tests/fixtures/file2.yml'
+NESTED_JSON_FILE1 = 'tests/fixtures/file1_nested.json'
+NESTED_JSON_FILE2 = 'tests/fixtures/file2_nested.json'
+NESTED_YAML_FILE1 = 'tests/fixtures/file1_nested.yaml'
+NESTED_YAML_FILE2 = 'tests/fixtures/file2_nested.yaml'
 
 ANSWER_STYLISH_FLAT = 'tests/fixtures/answer_stylish_flat'
-ANSWER_JSON_FLAT = 'tests/fixtures/answer_json_flat'
-ANSWER_PLAIN_FLAT = 'tests/fixtures/answer_plain_flat'
+ANSWER_STYLISH_NESTED = 'tests/fixtures/answer_stylish_nested'
 
 def get_answer(answer_path):
     return read_file(answer_path)
@@ -22,7 +25,9 @@ def get_answer(answer_path):
 @pytest.mark.parametrize('filepath1, filepath2, format_name, answer', [
     (FLAT_JSON_FILE1, FLAT_JSON_FILE2, STYLISH, ANSWER_STYLISH_FLAT),
     (FLAT_YAML_FILE1, FLAT_YAML_FILE2, STYLISH, ANSWER_STYLISH_FLAT),
-    (FLAT_YML_FILE1, FLAT_YML_FILE2, STYLISH, ANSWER_STYLISH_FLAT)
+    (FLAT_YML_FILE1, FLAT_YML_FILE2, STYLISH, ANSWER_STYLISH_FLAT),
+    (NESTED_JSON_FILE1, NESTED_JSON_FILE2, STYLISH, ANSWER_STYLISH_NESTED),
+    (NESTED_YAML_FILE1, NESTED_YAML_FILE2, STYLISH, ANSWER_STYLISH_NESTED),
 ])
 def test_generate_diff(filepath1, filepath2, format_name, answer):
     assert generate_diff(filepath1, filepath2, format_name) \
